de936ce898dfa06129a24fda40770830ac06bcdd
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2024-11-27 18:13:33 -0500

    url con https

--- src/api/environment.ts
+++ src/api/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080', // URL base de la API para desarrollo
+    apiUrl: 'https://grupoctc.ddns.net/backend-tecnico', // URL base de la API local
+
     //url para reportes en .net
     apiUrlReportes: 'https://localhost:7105/api/Clientes'
 
